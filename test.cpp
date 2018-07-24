e76b7a02755a2ec7fd35b58446212f88e7c93869
Derek Burnheim <derek@hemicube.com>
2018-07-24 17:58:58 +1000

    R2G3B3 test

--- test.cpp
+++ test.cpp
@@ -3,22 +3,88 @@
 class TestVgfw : public Vgfw
 {
 public:
-    bool on_create() override { return true; }
+    uint8_t make_color(float r, float g, float b)
+    {
+        int red_bits = (r == 1.0f) ? 3 : static_cast<int>(r * 4);
+        int green_bits = (g == 1.0f) ? 7 : static_cast<int>(g * 8);
+        int blue_bits = (b == 1.0f) ? 7 : static_cast<int>(b * 8);
+        return (red_bits << 6) | (green_bits << 3) | blue_bits;
+    }
+
+    bool on_create() override
+    {
+        uint8_t r2g3b3[768];
+
+        for (int i = 0; i < 256; ++i)
+        {
+            int red_bits = (i >> 6) & 3;
+            int green_bits = (i >> 3) & 7;
+            int blue_bits = i & 7;
+
+            r2g3b3[(i * 3) + 0] = (red_bits * 255) / 3;
+            r2g3b3[(i * 3) + 1] = (green_bits * 255) / 7;
+            r2g3b3[(i * 3) + 2] = (blue_bits * 255) / 7;
+        }
+
+        set_palette(r2g3b3);
+        return true;
+    }
 
     bool on_update(float delta) override
     {
-        for (int x = 0; x < 320; ++x)
+        if (m_keys[L' '].pressed)
         {
-            for (int y = 0; y < 240; ++y)
-            {
-                set_pixel(x, y, (x / 20) + (y / 15) * 16);
-            }
+            greyscale = !greyscale;
         }
 
+        if (!greyscale)
+        {
+            for (int x = 0; x < 320; ++x)
+            {
+                for (int y = 0; y < 240; ++y)
+                {
+                    float fx = x / 320.0f;
+                    float fy = y / 240.0f;
+
+                    float dr = sqrtf((0.25f - fx) * (0.25f - fx) + (0.25f - fy) * (0.25f - fy));
+                    dr = dr < 1.0f ? dr : 1.0f;
+                    float r = 1.0f - dr;
+
+                    float dg = sqrtf((0.75f - fx) * (0.75f - fx) + (0.25f - fy) * (0.25f - fy));
+                    dg = dg < 1.0f ? dg : 1.0f;
+                    float g = 1.0f - dg;
+
+                    float db = sqrtf((0.25f - fx) * (0.25f - fx) + (0.75f - fy) * (0.75f - fy));
+                    db = db < 1.0f ? db : 1.0f;
+                    float b = 1.0f - db;
+
+                    set_pixel(x, y, make_color(r * r, g * g, b * b));
+                }
+            }
+        }
+        else
+        {
+            for (int x = 0; x < 320; ++x)
+            {
+                for (int y = 0; y < 240; ++y)
+                {
+                    float fx = x / 320.0f;
+                    float fy = y / 240.0f;
+
+                    float dr = sqrtf((0.5f - fx) * (0.5f - fx) + (0.5f - fy) * (0.5f - fy));
+                    dr = dr < 1.0f ? dr : 1.0f;
+                    float r = (1.0f - dr) * (1.0f - dr);
+
+                    set_pixel(x, y, make_color(r, r, r));
+                }
+            }
+        }
         return true;
     }
 
     void on_destroy() override {}
+
+    bool greyscale = false;
 };
 
 int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
